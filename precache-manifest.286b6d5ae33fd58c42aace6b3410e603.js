512cce4be8ee12c6c91484137bcf00099c7d891b
Jonasz Potoniec <jonasz@ubuntu.ubuntu-domain>
2019-12-13 21:18:13 +0100

    fixed github pages

---
+++ precache-manifest.286b6d5ae33fd58c42aace6b3410e603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47d8d8c1dae03469a76d056bd057d0f",
+    "url": "/cards4children/index.html"
+  },
+  {
+    "revision": "2eaf17c120da16035c18",
+    "url": "/cards4children/static/css/main.7de8172c.chunk.css"
+  },
+  {
+    "revision": "2944bb61ed0ad10ab0ff",
+    "url": "/cards4children/static/js/2.a17c3a52.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cards4children/static/js/2.a17c3a52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2eaf17c120da16035c18",
+    "url": "/cards4children/static/js/main.dc530e26.chunk.js"
+  },
+  {
+    "revision": "d6ea763e1be287712532",
+    "url": "/cards4children/static/js/runtime-main.dd3451a6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe0129cf20a8fb4f873ccc438b3da2f3.js"
+  "/cards4children/precache-manifest.286b6d5ae33fd58c42aace6b3410e603.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cards4children/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
